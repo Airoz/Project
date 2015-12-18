997c072ef55c3c218fcbb4e5f83f02d425d1f7e8
Airoz <tyozzie2478@live.ca>
2015-12-18 17:43:26 -0500

    Final Project Completed
    Tyler Osborne
    Game Development 1 - 0NA
    Friday Mornings from 8:50am - 10:40am

---
+++ Final Project/Assets/Scripts/EnemyAIScripts.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAIScripts : MonoBehaviour
+{
+
+    public Transform player;
+    public float playerDistance;
+    public float rotationDamping = 15;
+    public float moveSpeed = 5;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        playerDistance = Vector3.Distance(player.position, transform.position);
+        // have the enemy look at the player
+        if (playerDistance < 150.0f)
+        {
+
+            lookAtPlayer();
+
+        }
+        if (playerDistance < 150.0f)
+        {
+
+            chasePlayer();
+
+        }
+        
+	}
+
+    void lookAtPlayer()
+    {
+
+        Quaternion rotation = Quaternion.LookRotation(player.position - transform.position);
+        //rotate the enemy
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationDamping);
+
+    }
+
+    void chasePlayer()
+    {
+
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+    }
+
+}
+
